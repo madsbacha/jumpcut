c1c03df0f8864f93f28c65a789ce6e0c034708fa
Mads <madsbaan@gmail.com>
2020-04-08 21:19:50 +0200

    Invert sections

--- main.py
+++ main.py
@@ -30,75 +30,57 @@ if __name__ == '__main__':
     if not path.isfile("audio.wav") and path.isfile("video.mp4"):
         extract_audio("video.mp4", "audio.wav")
 
-    tempdata, samplerate = sf.read('audio.wav')
-    data = tempdata[0:int(samplerate*20)]
+    data, samplerate = sf.read('audio.wav')
     peaks, _ = find_peaks(data, height=0)
     audio_limit = 0.01
+    padding = 0.25
 
-    masked_peaks = ma.masked_where(data[peaks] > audio_limit, peaks)
-    unmasked_peaks = ma.masked_where(data[peaks] < audio_limit, peaks)
+    masked_peaks = ma.masked_where(data[peaks] < audio_limit, peaks)
 
     clumped_masked_peaks = ma.flatnotmasked_contiguous(masked_peaks)
 
-    plt.plot(masked_peaks, data[masked_peaks], 'rx')
-    plt.plot(unmasked_peaks, data[unmasked_peaks], 'bx')
+    sections = [[
+            (peaks[clumped_masked_peaks[0].start] - samplerate * padding) / samplerate,
+            (peaks[clumped_masked_peaks[0].stop - 1] + samplerate * padding) / samplerate
+        ]]
+    
+    if (sections[0][0] < 0):
+        sections[0][0] = 0
 
-    filtered_clumps = []
-    removed_sections = []
-
-    for clump in clumped_masked_peaks:
-        if samplerate / 4 / 1000 < (clump.stop - clump.start):
-            filtered_clumps.extend(peaks[clump])
-            removed_sections.append([
-                peaks[clump.start] / samplerate,
-                peaks[clump.stop] / samplerate
+    for i in range(1, len(clumped_masked_peaks)):
+        if peaks[clumped_masked_peaks[i-1].stop - 1] + samplerate * (padding * 2) >= peaks[clumped_masked_peaks[i].start]:
+            sections[len(sections) - 1][1] = (peaks[clumped_masked_peaks[i].stop - 1] + samplerate * padding) / samplerate
+        else:
+            sections.append([
+                (peaks[clumped_masked_peaks[i].start] - samplerate * padding) / samplerate,
+                (peaks[clumped_masked_peaks[i].stop] + samplerate * padding) / samplerate
             ])
 
-    plt.plot(peaks[clumped_masked_peaks[0]], data[peaks[clumped_masked_peaks[0]]], 'gx')
-    # plt.plot(np.zeros_like(peaks), "--", color="gray")
-    plt.show()
-
-    max_names = len(removed_sections) + 1 + len(removed_sections) + 1
     out = []
-    name_i = 0
-    if removed_sections[0][0] > 0.0:
-        out.append(trim(0, removed_sections[0][0], hex(name_i)))
-        name_i += 1
+    for i, section in enumerate(sections):
+        out.append(trim(sections[i][0], sections[i][1], hex(i)))
 
-    for i in range(1, len(removed_sections)):
-        out.append(trim(removed_sections[i-1][1], removed_sections[i][0], hex(name_i)))
-        name_i += 1
-
-    out.append(trim(
-        removed_sections[len(removed_sections) - 1][1],
-        len(data) / samplerate,
-        hex(name_i)))
-    name_i += 1
-
+    name_i = len(sections)
     out.append(concat(
         hex(0),
         hex(1),
         hex(name_i)
     ))
     name_i += 1
-    for i in range(2, len(removed_sections)):
+
+    for i in range(2, len(sections)):
         out.append(concat(
             hex(name_i - 1),
             hex(i),
             hex(name_i)
         ))
         name_i += 1
-    out.append(concat(
-        hex(name_i - 1),
-        hex(len(removed_sections)),
-        hex(name_i)
-    ))
+    name_i -= 1
 
-    seperator = ';'
-    cmd = f"ffmpeg -i video.mp4 -y -filter_complex \"{seperator.join(out)}\" -map \"[v{hex(name_i)}]\" -map \"[a{hex(name_i)}]\" out.mp4"
+    cmd = f"ffmpeg -i video.mp4 -y -filter_complex \"{';'.join(out)}\" -map \"[v{hex(name_i)}]\" -map \"[a{hex(name_i)}]\" out.mp4"
 
-    with open('cmd.txt', 'w') as f:
-        f.write(cmd)
+    # with open('cmd.txt', 'w') as f:
+    #     f.write(cmd)
 
     # print(cmd)
 
