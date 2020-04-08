ee20df04a29845ad53c27451b48a13af10f56132
Mads <madsbaan@gmail.com>
2020-04-08 20:05:38 +0200

    Testing things

--- main.py
+++ main.py
@@ -1,112 +1,105 @@
 import soundfile as sf
-from math import floor
 from os import system, path
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+import numpy.ma as ma
+
 
 def extract_audio(video_file, audio_file="audio.wav"):
-  system(f"ffmpeg -i {video_file} -vn -acodec pcm_s16le -ar 44100 -ac 2 {audio_file}")
+    system(f"ffmpeg -i {video_file} -vn -acodec pcm_s16le -ar 44100 -ac 2 {audio_file}")
 
-def average(data, f, t):
-  if f >= t:
-    raise Exception('Argument error: f must be smaller than t')
-  val = 0
-  prev = 0
-  for i in range(f, t + 1):
-    if prev < abs(data[i]):
-      prev = abs(data[i])
-    else:
-      val += prev
-  return val / (t - f + 1)
 
 def trim(start, end, name):
-  start = round(start, 2)
-  end = round(end, 2)
-  out = ""
-  if start == 0.0:
-    out += f"[0:v]trim=duration={end}[v{name}];[0:a]atrim=duration={end}[a{name}]"
-  else:
-    out += f"[0:v]trim=start={start}:end={end},setpts=PTS-STARTPTS[v{name}];"
-    out += f"[0:a]atrim=start={start}:end={end},asetpts=PTS-STARTPTS[a{name}]"
-  return out
+    start = round(start, 2)
+    end = round(end, 2)
+    out = ""
+    if start == 0.0:
+        out += f"[0:v]trim=duration={end}[v{name}];[0:a]atrim=duration={end}[a{name}]"
+    else:
+        out += f"[0:v]trim=start={start}:end={end},setpts=PTS-STARTPTS[v{name}];"
+        out += f"[0:a]atrim=start={start}:end={end},asetpts=PTS-STARTPTS[a{name}]"
+    return out
+
 
 def concat(a, b, out):
-  return f"[v{a}][v{b}]concat[v{out}];[a{a}][a{b}]concat=v=0:a=1[a{out}]"
+    return f"[v{a}][v{b}]concat[v{out}];[a{a}][a{b}]concat=v=0:a=1[a{out}]"
 
-if not path.isfile("audio.wav") and path.isfile("video.mp4"):
-  extract_audio("video.mp4", "audio.wav")
 
-info = sf.info('audio.wav')
-second_sections = int(info.samplerate / 1024)
-blocksize = int(info.samplerate/second_sections)
+if __name__ == '__main__':
+    if not path.isfile("audio.wav") and path.isfile("video.mp4"):
+        extract_audio("video.mp4", "audio.wav")
 
-rms = [np.sqrt(np.mean(block**2)) for block in
-       sf.blocks('audio.wav', blocksize=blocksize, overlap=512)]
+    tempdata, samplerate = sf.read('audio.wav')
+    data = tempdata[0:int(samplerate*20)]
+    peaks, _ = find_peaks(data, height=0)
+    audio_limit = 0.01
 
-audio_limit = 0.01
+    masked_peaks = ma.masked_where(data[peaks] > audio_limit, peaks)
+    unmasked_peaks = ma.masked_where(data[peaks] < audio_limit, peaks)
 
-plt.plot(rms)
-x=np.linspace(0,len(rms), len(rms))
-plt.plot(x, x*0+audio_limit, '-r', label='audio limit')
-plt.show()
+    clumped_masked_peaks = ma.flatnotmasked_contiguous(masked_peaks)
 
-removed_sections = []
+    plt.plot(masked_peaks, data[masked_peaks], 'rx')
+    plt.plot(unmasked_peaks, data[unmasked_peaks], 'bx')
 
-new_section_start = None
-new_section_end = None
-new_section_count = 0
+    filtered_clumps = []
+    removed_sections = []
 
-for i, block in enumerate(rms):
-  if block < audio_limit:
-    print(block)
-    if new_section_count == 0:
-      new_section_start = blocksize * i - 1
-    new_section_end = blocksize * i
-    new_section_count += 1
-  elif new_section_count > 0:
-    if new_section_count >= (int(second_sections) * 0.75):
-      removed_sections.append([new_section_start / info.samplerate, new_section_end / info.samplerate])
-    new_section_start = None
-    new_section_end = None
-    new_section_count = 0
+    for clump in clumped_masked_peaks:
+        if samplerate / 4 / 1000 < (clump.stop - clump.start):
+            filtered_clumps.extend(peaks[clump])
+            removed_sections.append([
+                peaks[clump.start] / samplerate,
+                peaks[clump.stop] / samplerate
+            ])
 
-max_names = len(removed_sections) + 1 + len(removed_sections) + 1
-out = []
-name_i = 0
-if removed_sections[0][0] > 0.0: 
-  out.append(trim(0, removed_sections[0][0], hex(name_i)))
-  name_i += 1
+    plt.plot(peaks[clumped_masked_peaks[0]], data[peaks[clumped_masked_peaks[0]]], 'gx')
+    # plt.plot(np.zeros_like(peaks), "--", color="gray")
+    plt.show()
 
-for i in range(1, len(removed_sections)):
-  out.append(trim(removed_sections[i-1][1], removed_sections[i][0], hex(name_i)))
-  name_i += 1
+    max_names = len(removed_sections) + 1 + len(removed_sections) + 1
+    out = []
+    name_i = 0
+    if removed_sections[0][0] > 0.0:
+        out.append(trim(0, removed_sections[0][0], hex(name_i)))
+        name_i += 1
 
-out.append(trim(
-  removed_sections[len(removed_sections) - 1][1],
-  blocksize * len(rms) / info.samplerate,
-  hex(name_i)))
-name_i += 1
+    for i in range(1, len(removed_sections)):
+        out.append(trim(removed_sections[i-1][1], removed_sections[i][0], hex(name_i)))
+        name_i += 1
 
-out.append(concat(
-    hex(0),
-    hex(1),
-    hex(name_i)
+    out.append(trim(
+        removed_sections[len(removed_sections) - 1][1],
+        len(data) / samplerate,
+        hex(name_i)))
+    name_i += 1
+
+    out.append(concat(
+        hex(0),
+        hex(1),
+        hex(name_i)
     ))
-name_i += 1
-for i in range(2, len(removed_sections)):
-  out.append(concat(
-    hex(name_i - 1),
-    hex(i),
-    hex(name_i)
+    name_i += 1
+    for i in range(2, len(removed_sections)):
+        out.append(concat(
+            hex(name_i - 1),
+            hex(i),
+            hex(name_i)
+        ))
+        name_i += 1
+    out.append(concat(
+        hex(name_i - 1),
+        hex(len(removed_sections)),
+        hex(name_i)
     ))
-  name_i += 1
-name_i -= 1
 
-cmd = f"ffmpeg -i video.mp4 -filter_complex \"{';'.join(out)}\" -map \"[v{hex(name_i)}]\" -map \"[a{hex(name_i)}]\" out.mp4"
+    seperator = ';'
+    cmd = f"ffmpeg -i video.mp4 -y -filter_complex \"{seperator.join(out)}\" -map \"[v{hex(name_i)}]\" -map \"[a{hex(name_i)}]\" out.mp4"
 
-with open('cmd.txt', 'w') as f:
-  f.write(cmd)
+    with open('cmd.txt', 'w') as f:
+        f.write(cmd)
 
-# print(cmd)
+    # print(cmd)
 
-system(cmd)
+    system(cmd)
