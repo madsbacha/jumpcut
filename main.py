7cef1cc36d735f408b83c0b7f2ef61efa12f27d8
Mads <madsbaan@gmail.com>
2020-04-09 19:56:59 +0200

    Implement cli arguments

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import soundfile as sf
-from os import system, path
+from os import system, path, remove
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 import numpy.ma as ma
+import argparse
 
 
 def extract_audio(video_file, audio_file="audio.wav"):
-    system(f"ffmpeg -i {video_file} -vn -acodec pcm_s16le -ar 44100 -ac 1 {audio_file}")
+    system(f"ffmpeg -hide_banner -loglevel panic -i {video_file} -vn -acodec pcm_s16le -ar 44100 -ac 1 {audio_file}")
 
 
 def trim(start, end, name):
@@ -27,17 +28,47 @@ def concat(a, b, out):
 
 
 if __name__ == '__main__':
-    if not path.isfile("audio.wav") and path.isfile("video.mp4"):
-        extract_audio("video.mp4", "audio.wav")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-video', '-i', default="video.mp4", help='The location of the input video (default: %(default)s)')
+    parser.add_argument('-output', '-o', default="out.mp4", help='The location of the output video (default: %(default)s)')
+    parser.add_argument('-fps', default=15, help="Frame rate of the output video (default: %(default)s)")
+    parser.add_argument('-halfres', action='store_true', help="If present the output video will be half resolution")
+    parser.add_argument('-audio_limit', default=0.01, help='The threshold at which point the audio is removed (default: %(default)s)')
+    parser.add_argument('-section_padding', default=0.25, help='A multiple of one second to add to the beginning and end of each cut (default: %(default)s)')
+    parser.add_argument('-min_spacing', default=1.5, help='The minimum length between each cut (default: %(default)s)')
+    parser.add_argument('-audio', default='audio.wav', help='The program needs an audiofile to process where to cut. \
+        This specifies what audio file to process. \
+        Leave blank for automatic creation of said file. \
+        If the file does not exist, it will be created.')
+    parser.add_argument('-dry', action='store_true', help='Do not run the final ffmpeg command')
+    parser.add_argument('-print', action='store_true', help='Print the final ffmpeg command')
+    parser.add_argument('-save', help='Save the final ffmpeg command to the specified file')
+    parser.add_argument('-overwrite', action='store_true', help='Tell ffmpeg to overwrite output file if it already exists.')
+    args = parser.parse_args()
 
-    data, samplerate = sf.read('audio.wav')
+    audio_file = args.audio
+    video_file = args.video
+    output_file = args.output
+    audio_limit = args.audio_limit
+    padding = args.section_padding
+    max_section_spacing = args.min_spacing
+    dry = args.dry
+    fps = args.fps
+    halfres = args.halfres
+    print_cmd = args.print
+    save_cmd = args.save
+    overwrite = args.overwrite
+
+    remove_audio_file = False
+    if not path.isfile(audio_file) and path.isfile(video_file):
+        remove_audio_file = True
+        extract_audio(video_file, audio_file)
+
+    data, samplerate = sf.read(audio_file)
+    if remove_audio_file:
+        remove(audio_file)
     peaks, _ = find_peaks(data, height=0)
-    audio_limit = 0.01
-    padding = 0.25
-    max_section_spacing = 1.5
-
     masked_peaks = ma.masked_where(data[peaks] < audio_limit, peaks)
-
     clumped_masked_peaks = ma.flatnotmasked_contiguous(masked_peaks)
 
     sections = [[
@@ -76,13 +107,23 @@ if __name__ == '__main__':
             hex(name_i)
         ))
         name_i += 1
-    name_i -= 1
 
-    cmd = f"ffmpeg -i video.mp4 -y -preset veryfast -filter_complex \"{';'.join(out)};[v{hex(name_i)}]fps=fps=1,scale=w=-2:h=ih/2[v{hex(name_i+1)}]\" -map \"[v{hex(name_i+1)}]\" -map \"[a{hex(name_i)}]\" out.mp4"
+    final_audio_name_i = name_i - 1
 
-    # with open('cmd.txt', 'w') as f:
-    #     f.write(cmd)
+    out.append(f"[v{hex(name_i - 1)}]fps=fps={fps}[v{hex(name_i)}]")
+    name_i += 1
 
-    # print(cmd)
+    if halfres:
+        out.append(f"[v{hex(name_i - 1)}]scale=w=-2:h=ih/2[v{hex(name_i)}]")
+        name_i += 1
+    
+    cmd = f"ffmpeg -i {video_file} {'-y ' if overwrite else ''}-preset veryfast -filter_complex \"{';'.join(out)}\" -map \"[v{hex(name_i - 1)}]\" -map \"[a{hex(final_audio_name_i)}]\" {output_file}"
 
-    system(cmd)
+    if save_cmd:
+        with open(save_cmd, 'w') as f:
+            f.write(cmd)
+
+    if print_cmd:
+        print(cmd)
+    if not dry:
+        system(cmd)
