701b2269652a36dbea551644ae442694f6c89c81
Mads Andersen <mads@madsba.dk>
2021-10-08 13:51:29 +0200

    Use py-webrtcvad for VAD

--- README.md
+++ README.md
@@ -4,18 +4,22 @@ Remove all silence from a video
 ## Usage
 The file is meant to be used as a cli tool. You have to install the following packages:
 ```
-python -m pip install soundfile numpy scipy argparse
+python -m pip install soundfile argparse webrtcvad
+```
+
+### Example
+Following will produce `out.mp4`
+```
+python jumpcut.py -i video.mp4
 ```
 
 Here is the output of the help command:
 ```
 > python jumpcut.py --help
 
-usage: jumpcut.py [-h] [-video VIDEO] [-output OUTPUT] [-fps FPS] [-halfres]
-                  [-audio_limit AUDIO_LIMIT]
-                  [-section_padding SECTION_PADDING]
-                  [-min_spacing MIN_SPACING] [-audio AUDIO] [-dry] [-print]
-                  [-save SAVE] [-overwrite]
+usage: jumpcut.py [-h] [-video VIDEO] [-output OUTPUT] [-fps FPS] [-halfres] [-audio_limit AUDIO_LIMIT]
+                  [-section_padding SECTION_PADDING] [-min_spacing MIN_SPACING] [-audio AUDIO] [-dry] [-print]
+                  [-save SAVE] [-overwrite] [-vad_aggr VAD_AGGR]
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -26,21 +30,18 @@ optional arguments:
   -fps FPS              Frame rate of the output video (default: 15)
   -halfres              If present the output video will be half resolution
   -audio_limit AUDIO_LIMIT
-                        The threshold at which point the audio is removed
-                        (default: 0.01)
+                        The threshold at which point the audio is removed (default: 0.01)
   -section_padding SECTION_PADDING
-                        A multiple of one second to add to the beginning and
-                        end of each cut (default: 0.25)
+                        A multiple of one second to add to the beginning and end of each cut (default: 0.25)
   -min_spacing MIN_SPACING
                         The minimum length between each cut (default: 1.5)
-  -audio AUDIO          The program needs an audiofile to process where to
-                        cut. This specifies what audio file to process. Leave
-                        blank for automatic creation of said file. If the file
-                        does not exist, it will be created.
+  -audio AUDIO          The program needs an audiofile to process where to cut. This specifies what audio file to
+                        process. Leave blank for automatic creation of said file. If the file does not exist, it will
+                        be created.
   -dry                  Do not run the final ffmpeg command
   -print                Print the final ffmpeg command
   -save SAVE            Save the final ffmpeg command to the specified file
-  -overwrite            Tell ffmpeg to overwrite output file if it already
-                        exists.
+  -overwrite            Tell ffmpeg to overwrite output file if it already exists.
+  -vad_aggr VAD_AGGR    The aggressiveness mode for filtering out speech, using py-webrtcvad.
 ```
 
--- jumpcut.py
+++ jumpcut.py
@@ -1,13 +1,29 @@
-import soundfile as sf
+import collections
+import contextlib
+import wave
+
 from os import system, path, remove
-import numpy as np
-from scipy.signal import find_peaks
-import numpy.ma as ma
 import argparse
+import webrtcvad
 
 
 def extract_audio(video_file, audio_file="audio.wav"):
-    system(f"ffmpeg -hide_banner -loglevel panic -i {video_file} -vn -acodec pcm_s16le -ar 44100 -ac 1 {audio_file}")
+    system(f"ffmpeg -hide_banner -loglevel panic -i {video_file} -vn -acodec pcm_s16le -ar 32000 -ac 1 {audio_file}")
+
+
+def read_wave(path):
+    """Reads a .wav file.
+    Takes the path, and returns (PCM audio data, sample rate).
+    """
+    with contextlib.closing(wave.open(path, 'rb')) as wf:
+        num_channels = wf.getnchannels()
+        assert num_channels == 1
+        sample_width = wf.getsampwidth()
+        assert sample_width == 2
+        sample_rate = wf.getframerate()
+        assert sample_rate in (8000, 16000, 32000, 48000)
+        pcm_data = wf.readframes(wf.getnframes())
+        return pcm_data, sample_rate
 
 
 def trim(start, end, name):
@@ -26,15 +42,82 @@ def concat(a, b, out):
     return f"[v{a}][v{b}]concat[v{out}];[a{a}][a{b}]concat=v=0:a=1[a{out}]"
 
 
+class Frame(object):
+    """Represents a "frame" of audio data."""
+    def __init__(self, bytes, timestamp, duration):
+        self.bytes = bytes
+        self.timestamp = timestamp
+        self.duration = duration
+
+
+def frame_generator(frame_duration_ms, audio, sample_rate):
+    """Generates audio frames from PCM audio data.
+    Takes the desired frame duration in milliseconds, the PCM data, and
+    the sample rate.
+    Yields Frames of the requested duration.
+    """
+    n = int(sample_rate * (frame_duration_ms / 1000.0) * 2)
+    offset = 0
+    timestamp = 0.0
+    duration = (float(n) / sample_rate) / 2.0
+    while offset + n < len(audio):
+        yield Frame(audio[offset:offset + n], timestamp, duration)
+        timestamp += duration
+        offset += n
+
+
+def get_voice_segments(frames, frame_duration_ms, padding_duration_ms, sample_rate, vad):
+    num_padding_frames = int(padding_duration_ms / frame_duration_ms)
+    # We use a deque for our sliding window/ring buffer.
+    ring_buffer = collections.deque(maxlen=num_padding_frames)
+    # We have two states: TRIGGERED and NOTTRIGGERED. We start in the
+    # NOTTRIGGERED state.
+    triggered = False
+
+    voiced_segments = []
+    voiced_frames = []
+    for frame in frames:
+        webrtcvad.valid_rate_and_frame_length(sample_rate, len(frame.bytes))
+        is_speech = vad.is_speech(frame.bytes, sample_rate)
+
+        ring_buffer.append((frame, is_speech))
+
+        if not triggered:
+            num_voiced = len([f for f, speech in ring_buffer if speech])
+            # If we're NOTTRIGGERED and more than 90% of the frames in
+            # the ring buffer are voiced frames, then enter the
+            # TRIGGERED state.
+            if num_voiced > 0.9 * ring_buffer.maxlen:
+                triggered = True
+                # We want to yield all the audio we see from now until
+                # we are NOTTRIGGERED, but we have to start with the
+                # audio that's already in the ring buffer.
+                for f, s in ring_buffer:
+                    voiced_frames.append(f)
+                ring_buffer.clear()
+        else:
+            # We're in the TRIGGERED state, so collect the audio data
+            # and add it to the ring buffer.
+            voiced_frames.append(frame)
+
+            num_unvoiced = len([f for f, speech in ring_buffer if not speech])
+            # If more than 90% of the frames in the ring buffer are
+            # unvoiced, then enter NOTTRIGGERED and yield whatever
+            # audio we've collected.
+            if num_unvoiced > 0.9 * ring_buffer.maxlen:
+                triggered = False
+                ring_buffer.clear()
+                voiced_segments.append([voiced_frames[0].timestamp, voiced_frames[-1].timestamp])
+                voiced_frames = []
+    return voiced_segments
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-video', '-i', default="video.mp4", help='The location of the input video (default: %(default)s)')
     parser.add_argument('-output', '-o', default="out.mp4", help='The location of the output video (default: %(default)s)')
     parser.add_argument('-fps', default=15, help="Frame rate of the output video (default: %(default)s)")
     parser.add_argument('-halfres', action='store_true', help="If present the output video will be half resolution")
-    parser.add_argument('-audio_limit', default=0.01, help='The threshold at which point the audio is removed (default: %(default)s)')
-    parser.add_argument('-section_padding', default=0.25, help='A multiple of one second to add to the beginning and end of each cut (default: %(default)s)')
-    parser.add_argument('-min_spacing', default=1.5, help='The minimum length between each cut (default: %(default)s)')
     parser.add_argument('-audio', default='audio.wav', help='The program needs an audiofile to process where to cut. \
         This specifies what audio file to process. \
         Leave blank for automatic creation of said file. \
@@ -43,49 +126,32 @@ if __name__ == '__main__':
     parser.add_argument('-print', action='store_true', help='Print the final ffmpeg command')
     parser.add_argument('-save', help='Save the final ffmpeg command to the specified file')
     parser.add_argument('-overwrite', action='store_true', help='Tell ffmpeg to overwrite output file if it already exists.')
+    parser.add_argument('-vad_aggr', default=1, help='The aggressiveness mode for filtering out speech, using py-webrtcvad.')
     args = parser.parse_args()
 
     audio_file = args.audio
     video_file = args.video
     output_file = args.output
-    audio_limit = args.audio_limit
-    padding = args.section_padding
-    max_section_spacing = args.min_spacing
     dry = args.dry
     fps = args.fps
     halfres = args.halfres
     print_cmd = args.print
     save_cmd = args.save
     overwrite = args.overwrite
+    vad_aggr = int(args.vad_aggr)
 
     remove_audio_file = False
     if not path.isfile(audio_file) and path.isfile(video_file):
         remove_audio_file = True
         extract_audio(video_file, audio_file)
 
-    data, samplerate = sf.read(audio_file)
+    data, samplerate = read_wave(audio_file)
     if remove_audio_file:
         remove(audio_file)
-    peaks, _ = find_peaks(data, height=0)
-    masked_peaks = ma.masked_where(data[peaks] < audio_limit, peaks)
-    clumped_masked_peaks = ma.flatnotmasked_contiguous(masked_peaks)
 
-    sections = [[
-            (peaks[clumped_masked_peaks[0].start] - samplerate * padding) / samplerate,
-            (peaks[clumped_masked_peaks[0].stop - 1] + samplerate * padding) / samplerate
-        ]]
-    
-    if (sections[0][0] < 0):
-        sections[0][0] = 0
-
-    for i in range(1, len(clumped_masked_peaks)):
-        if peaks[clumped_masked_peaks[i-1].stop - 1] + samplerate * max_section_spacing >= peaks[clumped_masked_peaks[i].start]:
-            sections[len(sections) - 1][1] = (peaks[clumped_masked_peaks[i].stop - 1] + samplerate * padding) / samplerate
-        else:
-            sections.append([
-                (peaks[clumped_masked_peaks[i].start] - samplerate * padding) / samplerate,
-                (peaks[clumped_masked_peaks[i].stop] + samplerate * padding) / samplerate
-            ])
+    vad = webrtcvad.Vad(vad_aggr)
+    frames = list(frame_generator(30, data, samplerate))
+    sections = get_voice_segments(frames, 30, 300, samplerate, vad)
 
     out = []
     for i, section in enumerate(sections):
